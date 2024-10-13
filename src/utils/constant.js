9149a51d73ba2cc86a5060236ce6b9a36581a4de
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2024-10-13 10:20:21 +0530

    first commit

---
+++ src/utils/constant.js
@@ -0,0 +1,14 @@
+// export const USER_API_END_POINT="https://jobportal-kgcc.onrender.com/api/v1/user";
+// export const JOB_API_END_POINT="https://jobportal-kgcc.onrender.com/api/v1/job";
+// export const APPLICATION_API_END_POINT="https://jobportal-kgcc.onrender.com/api/v1/application";
+// export const COMPANY_API_END_POINT="https://jobportal-kgcc.onrender.com/api/v1/company";
+
+// export const USER_API_END_POINT="http://localhost:5000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:5000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:5000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:5000/api/v1/company";
+
+export const USER_API_END_POINT="https://jobpost-backend-53x9.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobpost-backend-53x9.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobpost-backend-53x9.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobpost-backend-53x9.onrender.com/api/v1/company";
